49809b01bb9acc42fa763adfecd789518d1b82f5
NOUMSI Ronald <114578109+Ronald202203@users.noreply.github.com>
2026-02-05 10:10:39 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Pour déploiement à la racine (username.github.io)
+  base: '/portfolio_ronald', // Pour déploiement à la racine (username.github.io)
 })
